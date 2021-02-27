00379e754d43bd9a20eefc0a0977f6de54bf4e13
xiaomomo <1170586741@qq.com>
2021-02-27 10:14:38 +0800

    加Add the configuration of creating database and table

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,47 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Describe: City class
+ * <p>
+ * Created by Ervin Liu on 2021/02/27---10:05
+ **/
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
